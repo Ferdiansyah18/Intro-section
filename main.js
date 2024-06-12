2dde88b1fd5b88bb4f75f504bc527b8850d0c580
Ferdiansyah <you@example.com>
2024-06-13 00:02:34 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let featuresMenu = document.getElementById("featuresDropDown");
+let companyMenu = document.getElementById("companyDropDown");
+
+function featDropBtn() {
+    featuresMenu.classList.toggle("active");
+}
+
+function companyDropBtn() {
+    companyMenu.classList.toggle("active");
+}
