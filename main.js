0f19e453fdc9846925799e66db3d6d5b44a0a80f
Ferdiansyah <you@example.com>
2024-06-13 20:21:38 +0700

    penambahan effect transition dan animattion

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 let featuresMenu = document.getElementById("featuresDropDown");
 let companyMenu = document.getElementById("companyDropDown");
+let featuresBtn = document.getElementById("featuresBtn");
+let companyBtn = document.getElementById("companyBtn");
 
 function featDropBtn() {
     featuresMenu.classList.toggle("active");
+    featuresBtn.classList.toggle("active")
 }
 
 function companyDropBtn() {
     companyMenu.classList.toggle("active");
+    companyBtn.classList.toggle("active")
 }
